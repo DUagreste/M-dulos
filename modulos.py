d6be624cf459dfc278b1ace524ad2aa3239be679
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-17 20:43:51 -0300

    Módulos e Pacotes.

--- modulos.py
+++ modulos.py
@@ -2,7 +2,7 @@
 # import random
 # import random as rd
 
-from random import (
+"""from random import (
     random,
     choice
 )
@@ -10,4 +10,21 @@ from random import (
 print(random())
 
 lista = ["Pedro", "Tiago", "João"]
-print(choice(lista))
+print(choice(lista))"""
+
+"""from random import randint
+
+print(randint(1, 5))"""
+
+"""
+Módulos - arquivos com extensão .py (arquivos python)
+Pacotes - diretórios contendo um conjunto de módulos
+"""
+
+from pacote import principal, secundario
+from pacote.subpacote import outros
+
+
+print(principal.soma(5, 7))
+print(secundario.quadratica(10))
+print(outros.cubica(3))
