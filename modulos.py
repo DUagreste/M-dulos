1e838c9bf11e22581b940e72cf02a1626baf937d
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-17 19:17:08 -0300

    first commit

---
+++ modulos.py
@@ -0,0 +1,13 @@
+# from random import choice
+# import random
+# import random as rd
+
+from random import (
+    random,
+    choice
+)
+
+print(random())
+
+lista = ["Pedro", "Tiago", "João"]
+print(choice(lista))
