13f4b925cc2b01034083b309c3e21f8cc2768482
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-22 19:40:39 -0300

    Módulos: objetos dunder

---
+++ dunder.py
@@ -0,0 +1,16 @@
+"""
+Objetos Dunder
+Dunder - Duplo underline
+Atributos mágicos - variáveis
+Métodos mágicos - funções
+
+__init__ : modelo antigo de criar pacote
+__name__ : verifica o nome do arquivo
+__main__
+__file__ : verifica o caminho/local do arquivo
+__doc__ : visualiza a documentação do arquivo
+
+"""
+
+
+print("olá mundo")
